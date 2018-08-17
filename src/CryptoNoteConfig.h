c03e17748ca15d745824d9319a8a287c1ce1bad2
rogerrobers <41271043+rogerrobers@users.noreply.github.com>
2018-08-17 12:25:40 -0400

    update  mixin
    set to block 93333

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -63,11 +63,14 @@ const uint64_t MINIMUM_MIXIN_V1                              = 0;
 const uint64_t MAXIMUM_MIXIN_V1                              = 7;
 const uint64_t MINIMUM_MIXIN_V2                              = 7;
 const uint64_t MAXIMUM_MIXIN_V2                              = 7;
+const uint64_t MINIMUM_MIXIN_V3                              = 3;
+const uint64_t MAXIMUM_MIXIN_V3                              = 3;
 
 const uint32_t MIXIN_LIMITS_V1_HEIGHT                        = 40;
 const uint32_t MIXIN_LIMITS_V2_HEIGHT                        = 60;
+const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 93333;
 
-const uint64_t DEFAULT_MIXIN                                 = MINIMUM_MIXIN_V2;
+const uint64_t DEFAULT_MIXIN                                 = MINIMUM_MIXIN_V3;
 
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10);
 const uint64_t DEFAULT_DUST_THRESHOLD_V2                     = UINT64_C(10);
